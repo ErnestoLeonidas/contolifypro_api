632050b28a96e4442e2fb32e483647fe3bf268cc
Ernesto Velasquez <ernestoleonidas@gmail.com>
2021-07-10 17:11:17 -0400

    Se cagregan rutas

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 
-from models import Usuarios, db
+from models import Usuarios, Empresas, Actividades, db
 
 app = Flask(__name__)
 
@@ -12,7 +12,7 @@ app.config['JWT_SECRET_KEY'] = "secret-key"
 
 db.init_app(app)
 
-@app.route('/')
+@app.route('/usuarios', methods=['POST','GET'])
 def test():
     user = Usuarios.query.all()
     # user = Usuarios.query.with_entities(Usuarios.primer_nombre).all()
@@ -25,6 +25,17 @@ def test():
     # user = Usuarios.query.get(1)
     # return jsonify(user.serialize()), 200
 
+@app.route('/empresas')
+def getEmpresas():
+    empresas = Empresas.query.all()
+    empresas = list(map(lambda x: x.serialize(), empresas))
+    return jsonify(empresas),200
+
+@app.route('/actividades')
+def getActividades():
+    actividades = Actividades.query.all()
+    actividades = list(map(lambda x: x.serialize(), actividades))
+    return jsonify(actividades),200
 
 
 app.run(host='localhost', port=5000)
