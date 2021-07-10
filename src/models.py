2485087bf512d790f73596ff2c0e965e209f7b57
Ernesto Velasquez <ernestoleonidas@gmail.com>
2021-07-09 23:14:45 -0400

    all classes

--- src/models.py
+++ src/models.py
@@ -4,6 +4,7 @@
 #   userdb: wankucl_controlifypro
 from logging import setLogRecordFactory
 from flask_sqlalchemy import SQLAlchemy
+# from datetime import datetime // se usa para setear el datetime https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/    ver simple realtionship
 db = SQLAlchemy()
 
 class Regiones(db.Model):
@@ -152,3 +153,157 @@ cargo_usuarios = db.Table('Cargo_Usuarios',
     db.Column('usuario_id', db.Integer, db.ForeignKey('Usuarios.id'), primary_key=True),
     db.Column('cargos_id', db.Integer, db.ForeignKey('Cargos.id'), primary_key=True)
 )
+
+class Localidades(db.Model):
+    __tablename__ = 'Localidades'
+    id = db.Column(db.Integer, primary_key=True)
+    sigla = db.Column(db.String(45), nullable=False)
+    nombre = db.Column(db.String(255), nullable=False)
+    estado = db.Column(db.Integer, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "sigla": self.sigla,
+            "nombre": self.nombre,
+            "estado": self.estado
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Proyectos(db.Model):
+    __tablename__ = 'Proyectos'
+    id = db.Column(db.Integer, primary_key=True)
+    sigla = db.Column(db.String(45), nullable=False)
+    nombre = db.Column(db.String(255), nullable=False)
+    descripcion = db.Column(db.String(255), nullable=False)
+    porcentaje_avance = db.Column(db.Integer, nullable=False)
+    presupuesto = db.Column(db.Integer, nullable=False)
+    fecha_inicio = db.Column(db.DateTime, nullable=True)
+    fecha_entrega = db.Column(db.DateTime, nullable=True)
+    estado = db.Column(db.Integer, nullable=False)
+
+    localidad_id = db.Column(db.Integer, db.ForeignKey('Localidades.id'), nullable=False)
+    localidad = db.relationship('Localidades', backref=db.backref('Proyectos', lazy=True))
+
+    jefe_proyecto_id = db.Column(db.Integer, db.ForeignKey('Usuarios.id'), nullable=False)
+    jefe_proyecto = db.relationship('Usuarios', backref=db.backref('Proyectos', lazy=True))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "sigla": self.sigla,
+            "nombre": self.nombre,
+            "descripcion": self.descripcion,
+            "porcentaje_avance": self.porcentaje_avance,
+            "presupuesto": self.presupuesto,
+            "fecha_inicio": self.fecha_inicio,
+            "fecha_entrega": self.fecha_entrega,
+            "estado": self.estado,
+            "localidad_id": self.localidad_id,
+            "jefe_proyecto_id": self.jefe_proyecto_id
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Actividades(db.Model):
+    __tablename__ = 'Actividades'
+    id = db.Column(db.Integer, primary_key=True)
+    descripcion = db.Column(db.String(255), nullable=False)
+    fecha_inicio = db.Column(db.DateTime, nullable=True)
+    porcentaje_avance = db.Column(db.Integer, nullable=False)
+    observacion = db.Column(db.String(2000), nullable=False)
+    estado = db.Column(db.Integer, nullable=False)
+
+    proyecto_id = db.Column(db.Integer, db.ForeignKey('Proyectos.id'), nullable=False)
+    proyecto = db.relationship('Proyectos', backref=db.backref('Actividades', lazy=True))
+
+    usuario_id = db.Column(db.Integer, db.ForeignKey('Usuarios.id'), nullable=False)
+    usuario = db.relationship('Usuarios', backref=db.backref('Actividades', lazy=True))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "descripcion": self.descripcion,
+            "fecha_inicio": self.fecha_inicio,
+            "porcentaje_avance": self.porcentaje_avance,
+            "observacion": self.observacion,
+            "estado": self.estado,
+            "proyecto_id": self.proyecto_id,
+            "usuario_id": self.usuario_id
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Horas(db.Model):
+    __tablename__ = 'Horas'
+    id = db.Column(db.Integer, primary_key=True)
+    descripcion = db.Column(db.String(255), nullable=False)
+    fecha = db.Column(db.DateTime, nullable=True)
+    hh = db.Column(db.Float, nullable=False)
+    hh_extra = db.Column(db.Float, nullable=False)
+    estado = db.Column(db.Integer, nullable=False)
+
+    actividad_id = db.Column(db.Integer, db.ForeignKey('Actividades.id'), nullable=False)
+    actividad = db.relationship('Actividades', backref=db.backref('Horas', lazy=True))
+    
+    usuario_id = db.Column(db.Integer, db.ForeignKey('Usuarios.id'), nullable=False)
+    usuario = db.relationship('Usuarios', backref=db.backref('Horas', lazy=True))
+
+    proyecto_id = db.Column(db.Integer, db.ForeignKey('Proyectos.id'), nullable=False)
+    proyecto = db.relationship('Proyectos', backref=db.backref('Horas', lazy=True))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "descripcion": self.descripcion,
+            "fecha": self.fecha,
+            "hh": self.hh,
+            "hh_extra": self.hh_extra,
+            "estado": self.estado,
+            "actividad_id": self.actividad_id,
+            "usuario_id": self.usuario_id,
+            "proyecto_id": self.proyecto_id
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+# usuarios_empresas
+
+# disciplinas
