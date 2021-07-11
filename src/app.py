cbf9e38fee873771dd1f90956e54740078e417b5
Ernesto Velasquez <ernestoleonidas@gmail.com>
2021-07-11 18:43:48 -0400

    actividades

--- src/app.py
+++ src/app.py
@@ -21,16 +21,22 @@ def getUsuarios():
     user = Usuarios.query.all()
     # user = Usuarios.query.with_entities(Usuarios.primer_nombre).all()
     user = list(map(lambda x: x.serialize(), user))
-
     return jsonify(user),200
 
 @app.route('/usuarios/<id>', methods=['GET'])
 def getUsuario(id):
     user = Usuarios.query.get(id)
+    return jsonify(user.serialize()),200
 
+@app.route('/usuarios/<id>', methods=['DELETE'])
+def deleteUsuario(id):
+    user = Usuarios.query.get(id)
+    estado = 0
+    user.estado = estado
+    Usuarios.update(user)
     return jsonify(user.serialize()),200
     
-@app.route('/usuarios/<id>', methods=['PUT'])
+@app.route('/usuarios/<id>', methods=['POST'])
 def updateUsuario(id):
     user = Usuarios.query.get(id)
 
@@ -62,12 +68,37 @@ def updateUsuario(id):
 
     return jsonify(user.serialize()),200
 
-@app.route('/usuarios/<id>', methods=['DELETE'])
-def deleteUsuario(id):
-    user = Usuarios.query.get(id)
-    estado = 0
+@app.route('/usuarios', methods=['PUT'])
+def addUsuario():
+
+    user = Usuarios()
+
+    rut = request.json.get('rut')
+    primer_nombre = request.json.get('primer_nombre')
+    segundo_nombre = request.json.get('segundo_nombre')
+    apellido_paterno = request.json.get('apellido_paterno')
+    apellido_materno = request.json.get('apellido_materno')
+    password = request.json.get('password')
+    email = request.json.get('email')
+    estado = request.json.get('estado')
+    avatar = request.json.get('avatar')
+    comuna_id = request.json.get('comuna_id')
+    rol_id = request.json.get('rol_id')
+
+    user.rut = rut
+    user.primer_nombre = primer_nombre
+    user.segundo_nombre = segundo_nombre
+    user.apellido_paterno = apellido_paterno
+    user.apellido_materno = apellido_materno
+    user.password = password
+    user.email = email
     user.estado = estado
-    Usuarios.update(user)
+    user.avatar = avatar
+    user.comuna_id = comuna_id
+    user.rol_id = rol_id
+
+    Usuarios.save(user)
+
     return jsonify(user.serialize()),200
 
 @app.route('/empresas', methods=['GET'])
@@ -81,6 +112,68 @@ def getActividades():
     actividades = Actividades.query.all()
     actividades = list(map(lambda x: x.serialize(), actividades))
     return jsonify(actividades),200
+
+@app.route('/actividades/<id>', methods=['GET'])
+def getActividad(id):
+    actividad = Actividades.query.get(id)
+    return jsonify(actividad.serialize()),200
+
+@app.route('/actividades/<id>', methods=['DELETE'])
+def deleteActividad(id):
+    actividad = Actividades.query.get(id)
+    estado = 0
+    Actividades.update(actividad)
+
+    return jsonify(actividad.serialize()),200
+
+@app.route('/actividades/<id>', methods=['POST'])
+def updateActividad(id):
+    actividad = Actividades.query.get(id)
+
+    descripcion = request.json.get('descripcion')
+    fecha_inicio = request.json.get('fecha_inicio')
+    porcentaje_avance = request.json.get('porcentaje_avance')
+    observacion = request.json.get('observacion')
+    estado = request.json.get('estado')
+    proyecto_id = request.json.get('proyecto_id')
+    usuario_id = request.json.get('usuario_id')
+
+    actividad.descripcion = descripcion
+    actividad.fecha_inicio = fecha_inicio
+    actividad.porcentaje_avance = porcentaje_avance
+    actividad.observacion = observacion
+    actividad.estado = estado
+    actividad.proyecto_id = proyecto_id
+    actividad.usuario_id = usuario_id
+
+    Actividades.update(actividad)
+
+    return jsonify(actividad.serialize()),200
+
+@app.route('/actividades', methods=['PUT'])
+def addActividad():
+
+    actividad = Actividades()
+
+    descripcion = request.json.get('descripcion')
+    fecha_inicio = request.json.get('fecha_inicio')
+    porcentaje_avance = request.json.get('porcentaje_avance')
+    observacion = request.json.get('observacion')
+    estado = request.json.get('estado')
+    proyecto_id = request.json.get('proyecto_id')
+    usuario_id = request.json.get('usuario_id')
+
+    actividad.descripcion = descripcion
+    actividad.fecha_inicio = fecha_inicio
+    actividad.porcentaje_avance = porcentaje_avance
+    actividad.observacion = observacion
+    actividad.estado = estado
+    actividad.proyecto_id = proyecto_id
+    actividad.usuario_id = usuario_id
+
+    Actividades.save(actividad)
+
+    return jsonify(actividad.serialize()),200
 
 @app.route('/proyectos', methods=['GET'])
 def getProyectos():
