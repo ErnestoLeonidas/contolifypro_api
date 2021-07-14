d37b51b48564da646ced6215129083b4f12b9211
Anthonny Tineo <anthonnytineo@gmail.com>
2021-07-13 22:21:08 -0400

    route de login

--- src/app.py
+++ src/app.py
@@ -1,7 +1,15 @@
+import os
 from flask import Flask, request, jsonify, render_template
 from flask_migrate import Migrate
 from flask_cors import CORS
 from models import db, Usuarios, Empresas, Actividades, Proyectos
+
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
 
 app = Flask(__name__)
 
@@ -196,6 +204,24 @@ def getProyectos():
     proyectos = list(map(lambda x: x.serialize(), proyectos))
     return jsonify(proyectos),200
 
+
+
+ 
+
+app.config["JWT_SECRET_KEY"] = "os.environ.get('super-secret')"  # No estoy creando una funcion para super-secret!
+jwt = JWTManager(app)
+
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@app.route("/token", methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    if email != "test" or password != "test":
+        return jsonify({"msg": "Bad email or password"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
 
 
 app.run(host='localhost', port=5000)
